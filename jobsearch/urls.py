9b31446671cf69a837b936c89aaaee88eff1236c
neptunhiker <bastiusa47@yahoo.de>
2024-04-11 08:37:14 +0200

    Create an udate view for an activity

--- jobsearch/urls.py
+++ jobsearch/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, ActivityDetailView
+from .views import HomePageView, ActivityDetailView, ActivityUpdateView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='jobsearch_home'),
     path('activity/<uuid:pk>', ActivityDetailView.as_view(), name='activity_detail'),
+    path('activity/edit/<uuid:pk>', ActivityUpdateView.as_view(), name='activity_edit'),
 ]
--- jobsearch/views.py
+++ jobsearch/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
 
 
 from .models import Activity
@@ -15,3 +15,10 @@ class ActivityDetailView(DetailView):
     template_name = 'jobsearch/activity_detail.html'
     context_object_name = 'activity'
     
+class ActivityUpdateView(UpdateView):
+    model = Activity
+    fields = ['headline', 'date', 'description', 'next_steps', 'reminder_date']
+    template_name = 'jobsearch/activity_update.html'
+    
+    def get_success_url(self) -> str:
+        return reverse("activity_detail", kwargs={"pk": self.object.pk})
